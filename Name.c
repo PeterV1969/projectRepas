bbf4a71f4662cf91800e456f1daca78b9e8472ea
PeterV1969 <valus.peter@centrum.sk>
2016-12-02 10:17:13 +0100

    Update Name.c

--- Name.c
+++ Name.c
@@ -4,7 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("Peter Valus");
+	printf("Peter ");
 	printf(" Peto, ak vidis tuto zmenu, daj mi nejako vediet ");
 	printf("nech viem, ze to funguje, Matus");
 	printf("teraz to funguje Peter");
@@ -12,3 +12,4 @@ int main(int argc, char *argv[]) {
 	printf("hela");
 	return 0;
 }
+
